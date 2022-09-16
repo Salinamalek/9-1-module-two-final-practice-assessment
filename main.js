04be72abdb55bcb246db3d198a129b5c91db0585
Salinamalek <salinamalek@pursuit.org>
2022-09-16 14:21:08 -0400

    name and age working

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
 const url = "https://ghibliapi.herokuapp.com/people"
 
+let people; 
+const select = document.querySelector("select");
+
+
 fetch(url)
-.then((res) => res.json())
-.then((resJson) => {
-    console.log(resJson)
+    .then((res) => res.json())
+    .then((resJson) => {
+    // console.log(resJson)
+    people = resJson
     for (let i = 0; i < resJson.length; i++) {
         const name = resJson[i].name
-        const select = document.querySelector("select")
-        const newOpt = document.createElement("option")
-        newOpt.textContent = name
-       select.append(newOpt);   
-    }
-})
-.catch((err) => console.log(err));
+        const newOpt = document.createElement("option");
+        newOpt.innerHTML = name;
+        newOpt.value = resJson[i].id
+        select.append(newOpt);  
+       }
+    })
+    .catch((err) => console.log(err));
+
+select.addEventListener("change", () => {
+    // console.log(select.value)
+    fetch(`${url}/${select.value}`)
+    .then((res) => res.json())
+    .then((resJson) => {
+        // console.log(resJson)
+        const h4 = document.querySelector('h4')
+        h4.innerHTML = ""
+        h4.innerHTML = resJson.name
+        const pAge = document.createElement('p')
+        pAge.innerHTML = resJson.age
+        h4.append(pAge)
+    })
+    .catch((err) => console.log(err))
+});
